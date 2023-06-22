520e70cf2b7df70fdd83eff4bed22b8318643ee9
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2023-06-22 22:23:01 +0100

    Added guest list JavaScript exercise, works but can be improved.

---
+++ java_notes/fundamentals_4/filling_guest_list/filling-in-guest-list.js
@@ -0,0 +1,19 @@
+const people = ['Chris', 'Anne', 'Colin', 'Terri', 'Phil', 'Lola', 'Sam', 'Kay', 'Bruce'];
+
+const admitted = document.querySelector('.admitted');
+const refused = document.querySelector('.refused');
+admitted.textContent = 'Admit: ';
+refused.textContent = 'Refuse: ';
+
+// loop starts here
+
+// refused.textContent += ;
+// admitted.textContent += ;
+
+for (const guest of people) {
+    if (guest == "Phil" || guest == "Lola") {
+        refused.textContent += `${guest}, `;
+    } else {
+        admitted.textContent += `${guest}, `;
+    }
+}
