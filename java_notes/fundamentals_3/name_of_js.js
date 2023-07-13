0a02ab863d7f164a87f3ff967b4b915c93ae4cdd
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2023-07-13 22:22:50 +0100

    Removed JavaScript notes to their own repo.

--- java_notes/fundamentals_3/name_of_js.js
+++
@@ -1,9 +0,0 @@
-let jsName = prompt("What is the official name of JavaScript? ''");
-
-if(jsName == "ECMAScript") {
-    alert("Correct!");
-} else if(jsName == null || undefined) {
-    alert("Cancelled.")
-} else {
-    alert("Incorrect, the answer is ECMAScript.")
-}
